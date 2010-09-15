5b7c34aec09812265fd3348281dcc197f6d23132
Bryan Berg <bryan@mixedmedialabs.com>
2010-09-15 16:44:02 -0700

    rewrite a bunch of this.

--- apns.js
+++ apns.js
@@ -1,110 +1,163 @@
-var sys = require("sys"),
-    net = require("net"),
-    fs  = require("fs"),
- crypto = require('crypto'),
- Buffer = require('buffer').Buffer
+var sys = require('sys'),
+net = require('net'),
+fs = require('fs'),
+crypto = require('crypto'),
+Buffer = require('buffer').Buffer,
+events = require('events');
 
 exports.createServer = function(pem_path, host, port) {
-  var options = {};
-  options.pem_path = pem_path;
-  options.host = host || 'gateway.sandbox.push.apple.com';
-  options.port = port || 2195;
-  
-  var server = new APNS(options);
- 
-  return server
+    var options = {};
+    options.pem_path = pem_path;
+    options.host = host || 'gateway.sandbox.push.apple.com';
+    options.port = port || 2195;
+
+    var server = new APNS(options);
+
+    return server
 }
 
 APNS = function(options) {
-  var self = this;
-  var pem = fs.readFileSync(options.pem_path, 'ascii');
-  
-  var client = this.client = net.createConnection(options.port, options.host);
-  this.waiting_buffers = [];
+    events.EventEmitter.call(this);
+    var self = this;
+    var pem = fs.readFileSync(options.pem_path, 'ascii');
 
-  var credentials = crypto.createCredentials({ca:pem, key:pem, cert:pem});
+    var client = this.client = net.createConnection(options.port, options.host);
 
-  client.setEncoding("UTF8");
-  client.setNoDelay(true)
-  client.setKeepAlive(true)
+    var credentials = crypto.createCredentials({
+        ca: pem,
+        key: pem,
+        cert: pem
+    });
 
-  client.addListener("connect", function () {
-    sys.puts("APNS connected.");
-    client.setSecure(credentials);
-  });
+    client.setEncoding('utf8');
+    client.setNoDelay(true);
+    client.setKeepAlive(true);
 
-  client.addListener("secure", function () {
-    self.waiting_buffers.forEach(function(b){
-      client.write(b);
-    })
-  });
+    client.on('connect', function() {
+        sys.puts('APNS: Connected.');
+        client.setSecure(credentials);
+    });
 
-  client.addListener("error", function (chunk) {
-    sys.puts("APNS ERROR: " + chunk);
-  });
+    client.on('secure', function() {
+        console.log('APNS: Secure.');
+        self.emit('connect');
+    });
 
-  client.addListener("end", function () {
-    sys.puts("APNS disconnected.");
-  });
+    client.on('error', function(exception) {
+        sys.puts('APNS: ' + exception.toString());
+        self.emit('end');
+    });
 
-  client.addListener("close", function () {
-    sys.puts("APNS closes.");
-  });
-}
+    client.on('end', function() {
+        sys.puts('APNS: Disconnected.');
+        client.end();
+        self.emit('end');
+    });
 
-// Notify device
-// 
-// JSON Object keys:
-//   alert
-//   badge
-//   sound
-// 
-// TODO custom keys
-//
-// usage: apns_instance.notify('12345abcdefg1235abcdefg', {"alert":"Hello World!"});
+    client.on('close', function() {
+        sys.puts('APNS: Closed.');
+    });
 
-APNS.prototype.notify = function(device_id, obj) {
-  var json = JSON.stringify({'aps':obj});
-  
-  var buffer = this.notification_buffer(device_id, json);
-  if (this.client.readyState == 'open') {
-    this.client.write(buffer);
-  } else {
-    this.waiting_buffers.push(buffer)
-  }
-  
-  return this;
-}
-
-APNS.prototype.HEXpack = function(str){
-  var p=[];
-  for (var i=0;i<str.length;i=i+2) { p.push(parseInt(str[i]+str[i+1], 16)); }
-  return p
-}
-
-APNS.prototype.ASCIIpack = function(str){
-  var p=[];
-  for (var i=0;i<str.length;i++) { p.push(str.charCodeAt(i)); }
-  return p;
-}
-
-APNS.prototype.notification_buffer = function(device_id, json) {
-  var a = [0,0,32].concat( this.HEXpack(device_id), [0,json.length], this.ASCIIpack(json) );
-    
-  var buffer = new Buffer(a.length);
-  var count = 0;
-  a.forEach(function(e) {
-   buffer[count] = e;
-   count++;
-  });
-  
-  return buffer;
+    client.on('data', function(data) {
+        // buffer message
+        // emit error
+        sys.puts('DATA! ' + self.ASCIIpack(data));
+    });
 }
 
 
 APNS.prototype.end = function() {
-  this.client.end();
+    this.client.end();
 }
+
+sys.inherits(APNS, events.EventEmitter);
+
+
+APNSMessage = function() {
+
+}
+
+APNSMessage.prototype.hex_pack = function(str) {
+    var p = [];
+    for (var i = 0; i < str.length; i = i + 2) {
+        p.push(parseInt(str[i] + str[i + 1], 16));
+    }
+    return p
+}
+
+APNSMessage.prototype.ascii_pack = function(str) {
+    var p = [];
+    for (var i = 0; i < str.length; i++) {
+        p.push(str.charCodeAt(i));
+    }
+    return p;
+}
+
+APNSMessage.prototype.uint32_to_bytes = function(i) {
+    var p = [];
+
+    p.push((i >> 24) & 0xff);
+    p.push((i >> 16) & 0xff);
+    p.push((i >> 8) & 0xff);
+    p.push((i >> 0) & 0xff);
+
+    return p;
+}
+
+PushMessage = function(identifier, expiry, device_id, obj) {
+    APNSMessage.call(this);
+
+    var self = this;
+
+    self.identifier = identifier;
+    self.expiry = expiry;
+    self.device_id = device_id;
+    self.obj = obj;
+}
+
+PushMessage.prototype.to_bytes = function() {
+    var self = this;
+
+    var json = JSON.stringify(self.obj);
+
+    var a = [];
+    a = a.concat(1, self.uint32_to_bytes(self.identifier), self.uint32_to_bytes(self.expiry));
+    a = a.concat([0, 32], self.hex_pack(self.device_id), [0, json.length], self.ascii_pack(json));
+
+    var buffer = new Buffer(a.length);
+    var count = 0;
+    a.forEach(function(e) {
+        buffer[count] = e;
+        count++;
+    });
+
+    return buffer;
+}
+
+PushMessage.send = function(service) {
+    service.client.write(this.to_bytes());
+}
+
+
+sys.inherits(PushMessage, APNSMessage);
+
+ErrorReply = function(buffer) {
+    APNSMessage.call(this);
+
+    var self = this;
+
+}
+
+sys.inherits(ErrorReply, APNSMessage);
+
+FeedbackReply = function(buffer) {
+    APNSMessage.call(this);
+
+    var self = this;
+
+}
+
+sys.inherits(FeedbackReply, APNSMessage);
 
 
 
